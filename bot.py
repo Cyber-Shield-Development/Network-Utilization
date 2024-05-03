71a133ac019ff90bc46e507a1bea2bb11a0767bf
Algorithms <142917637+advancedalgorithm@users.noreply.github.com>
2024-05-03 15:38:23 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,11 +7,25 @@
 """
 import discord
 
+class Config:
+    prefix = "-"
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
+
+        data = message.content
+        cmd = data
+        args = []
+        if not data.startswith(Config.prefix): return
+        if " " in data:
+            args = data.split(" ")
+            cmd = args[0]
+
+        if cmd == f"{Config.prefix}help":
+            await message.channel.send("WORKING")
         print(f'Message from {message.author}: {message.content}')
 
 
@@ -19,4 +33,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('')
+client.run('MTIzNjA0MzgxMDM3MTI3NjgzMA.GAaz1l.Rsavj8et4rgwllDlHzFUQrJxAFq-OG4qGOrwG0')
